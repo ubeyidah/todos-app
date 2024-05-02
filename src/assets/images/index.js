1c06b69fdbc39fae15d267e22bd3c1640eb930c5
ubeyid oumer <ubeyidah@gmail.com>
2024-05-02 21:15:03 +0300

    install reactjs and files

---
+++ src/assets/images/index.js
@@ -0,0 +1,12 @@
+import checkIcon from './icon-check.svg';
+import crossIcon from './icon-cross.svg';
+import moonIcon from './icon-moon.svg';
+import sunIcon from './icon-sun.svg';
+
+
+export {
+  checkIcon,
+  crossIcon,
+  moonIcon,
+  sunIcon
+}
