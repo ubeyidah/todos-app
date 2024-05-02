beeea81f30c775f82ce515d4dd7b32633596454a
ubeyid oumer <ubeyidah@gmail.com>
2024-05-02 21:34:50 +0300

    header and todo add input ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { moonIcon } from "./assets/images";
 import "./index.css";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <main className="container">
+      <header>
+        <h1 className="title">TODO</h1>
+        <button className="center">
+          <img src={moonIcon} alt="theme icon" />
+        </button>
+      </header>
+
+      <section className="inputs">
+        <div className="check-box"></div>
+        <input type="text" placeholder="Create a new todo..." />
+      </section>
+    </main>
+  );
 }
